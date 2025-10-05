1a204488ad209d01302d34614876f3bd6d5ccea2
finmook <apimooksarovath@gmail.com>
2025-10-05 11:30:32 +0700

    quiz 2

--- quiz2/package_some_move (1)/student.h
+++ quiz2/package_some_move (1)/student.h
@@ -5,10 +5,29 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
+//problem: insert the value in vector in our CP::vector but our CP::vector is like vector some move
+//solution: shift the value to the the right then insert(not optimal solution)
 template <typename T>
 void CP::vector_some_move<T>::insert(int index, std::vector<T> &value) {
   // Your code here
+  // std::vector<std::vector<T>> mData2;
+  // size_t newSize=
+ 
+  int j=(mSize-1);
+  ensureCapacity(mSize+value.size());
+  int newSize=mSize+value.size();
+  int i=newSize-1;
+  while(j>=index){
+    (*this)[i--]=(*this)[j--];
+  }
+  
+  int vidx=value.size()-1;
+  while(vidx>=0){
+    (*this)[i--]=value[vidx--];
+    
+  }
+  mSize=newSize;
+
 }
 
 #endif
